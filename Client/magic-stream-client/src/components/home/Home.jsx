edd399c69a95998d079b05a7fd3a4b2f6c000809
likalikali <ray.zhang.ruiji@foxmail.com>
2026-01-14 00:07:25 +0800

    fix errors

--- Client/magic-stream-client/src/components/home/Home.jsx
+++ Client/magic-stream-client/src/components/home/Home.jsx
@@ -1,40 +1,47 @@
-import { useState, useEffect } from "react";
-import axiosClient from "../../api/axiosConfig";
-import Movies from "../movies/movies.jsx";
+import {useState, useEffect} from 'react';
+import axiosClient from '../../api/axiosConfig'
+import Movies from '../movies/Movies';
 import Spinner from '../spinner/Spinner';
 
-const Home = ({updateMovieReview}) => {
+const Home =({updateMovieReview}) => {
     const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
     const [message, setMessage] = useState();
+
     useEffect(() => {
         const fetchMovies = async () => {
             setLoading(true);
             setMessage("");
             try{
-                const response = await axiosClient.get("/movies");
+                const response = await axiosClient.get('/movies');
                 setMovies(response.data);
-                if (response.data.length === 0) {
-                    setMessage("There are currently no movies available.");
+                if (response.data.length === 0){
+                    setMessage('There are currently no movies available')
                 }
+
             }catch(error){
-                console.error("Error fetching movies:", error);
-                setMessage("Error fetching movies");
+                console.error('Error fetching movies:', error)
+                setMessage("Error fetching movies")
             }finally{
                 setLoading(false)
             }
         }
         fetchMovies();
-},[])
-return (
-    <>
-        {loading ? (
-            <h2>Loading...</h2>
-        ) : (
-            <Movies movies={movies} updateMovieReview={updateMovieReview}  message={message}/>
+    }, []);
 
-        )}
-    </>
-);
-}
+    return (
+        <>
+            {loading ? (
+                <Spinner/>
+            ):  (
+                <Movies movies ={movies} updateMovieReview={updateMovieReview} message ={message}/>
+            )}
+        </>
+
+    );
+
+};
+
 export default Home;
+
+
